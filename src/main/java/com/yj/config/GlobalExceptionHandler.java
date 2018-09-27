90307da62cadb574aea5d63e30f06257d04a6341
yj1573 <439174370@qq.com>
2018-09-27 16:16:26 +0800

    日志配置修改记录错误日志

--- src/main/java/com/yj/config/GlobalExceptionHandler.java
+++ src/main/java/com/yj/config/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.yj.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public RModel defultExcepitonHandler(HttpServletRequest request, Exception e) {
         e.printStackTrace();
-
+        log.error("服务器异常",e);
         return  RModel.err().setData(e);
     }
+
 }
